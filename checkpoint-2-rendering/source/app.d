dbb1dfa6c5a91d91e297061957235b13ba40ad85
Peter Scamman <pscamman@gmail.com>
2016-04-17 22:39:45 -0500

    Attempt at updating chckpoint-2

--- checkpoint-2-rendering/source/app.d
+++ checkpoint-2-rendering/source/app.d
@@ -3,64 +3,16 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-import std.stdio;
+import std.stdio, std.typecons, std.string;
 import std.algorithm: canFind, count, filter, min, remove; // (1)
 import std.math: fmod, PI; // (2)
-import gfm.math, gfm.sdl2; // (3)
-import std.logger; // std.experimental.logger in newer versions
+import gfm.math, gfm.sdl2, gfm.logger; // (3)
+import std.experimental.logger;
 
 
 // Compile-time constants
 enum vec2i gameArea  = vec2i(800, 600);
 enum vec2f gameAreaF = vec2f(800.0f, 600.0f);
-
-struct GamePlatform
-{
-    SDL2 sdl2;             // Main SDL2 library, wrapped by gfm
-    SDLTTF sdlttf;         // SDL2 extension for font handling
-    SDL2Window window;     // Main game window
-    SDL2Renderer renderer; // Simple SDL2 builtin 2D renderer
-    SDLFont font;          // Font for our game
-
-    // Disable the default constructor
-    @disable this();
-
-    this(Logger log)
-    {
-        sdl2   = new SDL2(log);
-        scope(failure) { sdl2.close(); }
-        sdlttf = new SDLTTF(sdl2);
-        scope(failure) { sdlttf.close(); }
-
-        // Hide mouse cursor
-        SDL_ShowCursor(SDL_DISABLE);
-
-        // Open the game window.
-        const windowFlags = SDL_WINDOW_SHOWN | SDL_WINDOW_INPUT_FOCUS | SDL_WINDOW_MOUSE_FOCUS;
-        window = new SDL2Window(sdl2, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                                gameArea.x, gameArea.y, windowFlags);
-        scope(failure) { window.close(); }
-
-        // SDL renderer. For 2D drawing, this is easier to use than OpenGL.
-        renderer = new SDL2Renderer(window, SDL_RENDERER_ACCELERATED); // SDL_RENDERER_SOFTWARE
-        scope(failure) { renderer.close(); }
-
-        // Load the font.
-        import std.file: thisExePath;
-        import std.path: buildPath, dirName;
-        font = new SDLFont(sdlttf, thisExePath.dirName.buildPath("DroidSans.ttf"), 20);
-        scope(failure) { font.close(); }
-    }
-
-    ~this()
-    {
-        font.close();
-        renderer.close();
-        window.close();
-        sdlttf.close();
-        sdl2.close();
-    }
-}
 
 struct Entity
 {
@@ -165,59 +117,82 @@ void main()
 {
     writeln("Edit source/app.d to start your project.");
 
-
-    auto log = new FileLogger("asteroids-log.txt", "Asteroids log");
+    auto log = scoped!FileLogger("asteroids-log.txt");
 
     // Note: Many of the SDL init functions may fail and throw exceptions. In a real game,
     // this should be handled (e.g. a fallback renderer if accelerated doesn't work).
-    auto platform = GamePlatform(log);
 
-    import std.datetime: Clock;
-    // Last time we checked FPS
-    ulong prevFPSTime = Clock.currStdTime();
-    // Number of frames since last FPS update
-    uint frames = 0;
+    try {
+        auto sdl2   = scoped!SDL2(log);
+        auto sdlttf = scoped!SDLTTF(sdl2);
+
+        // Hide mouse cursor
+        SDL_ShowCursor(SDL_DISABLE);
+
+        // Open the game window.
+        const windowFlags = SDL_WINDOW_SHOWN | SDL_WINDOW_INPUT_FOCUS | SDL_WINDOW_MOUSE_FOCUS;
+        auto window = scoped!SDL2Window(sdl2, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                gameArea.x, gameArea.y, windowFlags);
+
+        // SDL renderer. For 2D drawing, this is easier to use than OpenGL.
+        auto renderer = scoped!SDL2Renderer(window, SDL_RENDERER_ACCELERATED); // SDL_RENDERER_SOFTWARE
+
+        // Load the font.
+        import std.file: thisExePath;
+        import std.path: buildPath, dirName;
+        auto font = scoped!SDLFont(sdlttf, thisExePath.dirName.buildPath("DroidSans.ttf"), 20);
+
+        import std.datetime: Clock;
+        // Last time we checked FPS
+        ulong prevFPSTime = Clock.currStdTime();
+        // Number of frames since last FPS update
+        uint frames = 0;
 
 
-    // Time when the last frame started (in hectonanoseconds, or 10ths of a microsecond)
-    ulong prevTime = prevFPSTime;
-    auto game = new GameState();
+        // Time when the last frame started (in hectonanoseconds, or 10ths of a microsecond)
+        ulong prevTime = prevFPSTime;
+        auto game = scoped!GameState();
 
-    mainLoop: while(true)
+        mainLoop: while(true)
+        {
+            const currTime = Clock.currStdTime();
+
+            ++frames;
+
+            const timeSinceFPS = currTime - prevFPSTime;
+            game.frameTimeSecs  = (currTime - prevTime) / 10_000_000.0;
+            prevTime = currTime;
+
+            // Update FPS every 0.1 seconds/1000000 hectonanoseconds
+            if(timeSinceFPS > 1_000_000)
+            {
+                const fps = frames / (timeSinceFPS / 10_000_000.0);
+                window.setTitle(format("Asteroids: %.2f FPS", fps));
+                frames = 0;
+                prevFPSTime = currTime;
+            }
+
+
+            SDL_Event event;
+            while(SDL_PollEvent(&event))
+            {
+                if(event.type == SDL_QUIT) { break mainLoop; }
+            }
+
+            // Fill the entire screen with black (background) color.
+            renderer.setColor(0, 0, 0, 0);
+            renderer.clear();
+
+            // Following draws will be white.
+            renderer.setColor(255, 255, 255, 255);
+            entityRendering(game.objects, renderer);
+
+            // Show the drawn result on the screen (swap front/back buffers)
+            renderer.present();
+        }
+    }
+    catch(Exception e)
     {
-        const currTime = Clock.currStdTime();
-
-        ++frames;
-
-        const timeSinceFPS = currTime - prevFPSTime;
-        game.frameTimeSecs  = (currTime - prevTime) / 10_000_000.0;
-        prevTime = currTime;
-
-        // Update FPS every 0.1 seconds/1000000 hectonanoseconds
-        if(timeSinceFPS > 1_000_000)
-        {
-            const fps = frames / (timeSinceFPS / 10_000_000.0);
-            platform.window.setTitle("Asteroids: %.2f FPS".format(fps));
-            frames = 0;
-            prevFPSTime = currTime;
-        }
-
-
-        SDL_Event event;
-        while(SDL_PollEvent(&event))
-        {
-            if(event.type == SDL_QUIT) { break mainLoop; }
-        }
-
-        // Fill the entire screen with black (background) color.
-        platform.renderer.setColor(0, 0, 0, 0);
-        platform.renderer.clear();
-
-        // Following draws will be white.
-        platform.renderer.setColor(255, 255, 255, 255);
-        entityRendering(game.objects, platform.renderer);
-
-        // Show the drawn result on the screen (swap front/back buffers)
-        platform.renderer.present();
+        writefln("%s", e.msg);
     }
 }
